284849d7e12d71dade1ef3610c44f03c8224e59d
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2015-10-15 14:36:52 +0200

    Task1

---
+++ Task1/Method.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSU.EPAM._1501.Day1.Bychkovksy.Task1
+{
+    public class Method
+    {
+        private readonly static double eps = 0.00001;
+        /// <summary>
+        /// calculating the roots of Newton's method
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="pow"></param>
+        /// <returns>double</returns>
+        public static double Newton(double number, double pow)
+        {
+            double x = ((pow - 1) * number + number / Math.Pow(number, pow - 1)) / pow;
+            double xn;
+            double temp;
+            do
+            {
+                xn = ((pow - 1) * x + number / Math.Pow(x, pow - 1)) / pow;
+                temp = x;
+                x = xn;
+            } while (Math.Abs((temp - xn)) > eps);
+
+            return temp;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BSU.EPAM._1501.Day1.Bychkovksy.Task1;
+
+namespace BSU.EPAM._1501.Day1.Bychkovsky.Task1Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Root of 2-th degree of 49 with Math.Pow = " + Math.Pow(49, 0.5));
+            Console.WriteLine("Root of 2-th degree of 49 with Newton's method =" + Method.Newton(49, 2));
+
+            Console.WriteLine("Root of 4-th degree of 20 with Math.Pow =  " + Math.Pow(20, 0.25));
+            Console.WriteLine("Root of 4-th degree of 49 with Newton's method  " + Method.Newton(20, 4));
+
+            Console.WriteLine("Root of 5-th degree of 100 with Math.Pow =  " + Math.Pow(100, 0.2));
+            Console.WriteLine("Root of 5-th degree of 100 with Newton's method  " + Method.Newton(100, 5));
+
+            Console.ReadLine();
+        }
+    }
+}
